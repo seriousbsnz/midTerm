e08070c4f58f7b0e6e0b8409e22402a14b8e7399
seriousbsnz <seriousbsnz@gmail.com>
2016-08-15 17:41:52 +0000

    mid term done

---
+++ module.js
@@ -0,0 +1,116 @@
+var app = angular.module("myApp", []).controller('myCtrl', function($scope) {
+    
+    $scope.interact = interact('.draggable')
+        .draggable({
+            inertia: true,
+            autoScroll: true,
+
+            // call this function on every dragmove event
+            onmove: dragMoveListener,
+            // call this function on every dragend event
+            onend: function(event) {
+                var textEl = event.target.querySelector('p');
+
+                textEl && (textEl.textContent =
+                    'moved a distance of ' +
+                    (Math.sqrt(event.dx * event.dx +
+                        event.dy * event.dy) | 0) + 'px');
+            }
+        });
+
+    function dragMoveListener(event) {
+        var target = event.target,
+            // keep the dragged position in the data-x/data-y attributes
+            x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+            y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+        // translate the element
+        target.style.webkitTransform =
+            target.style.transform =
+            'translate(' + x + 'px, ' + y + 'px)';
+
+        // update the posiion attributes
+        target.setAttribute('data-x', x);
+        target.setAttribute('data-y', y);
+    }
+
+    window.dragMoveListener = dragMoveListener;
+
+
+
+
+    $(document).ready(function() {
+        $("#btn1").click(function() {
+            $("img#1").hide(3000);
+        });
+        $("#btn2").click(function() {
+            $("img#1").show(3000);
+        });
+    });
+
+    $(document).ready(function() {
+        $("#btn3").click(function() {
+            $("img#2").hide(3000);
+        });
+        $("#btn4").click(function() {
+            $("img#2").show(3000);
+        });
+    });
+        $(document).ready(function() {
+        $("#btn5").click(function() {
+            $("img#3").hide(3000);
+        });
+        $("#btn6").click(function() {
+            $("img#3").show(3000);
+        });
+    });
+
+    $(document).ready(function() {
+        $("#btn7").click(function() {
+            $("img#4").hide(3000);
+        });
+        $("#btn8").click(function() {
+            $("img#4").show(3000);
+        });
+    });
+
+
+
+
+
+
+    interact('.resize-drag')
+        .draggable({
+            onmove: window.dragMoveListener
+        })
+        .resizable({
+            preserveAspectRatio: true,
+            edges: {
+                left: true,
+                right: true,
+                bottom: true,
+                top: true
+            }
+        })
+        .on('resizemove', function(event) {
+            var target = event.target,
+                x = (parseFloat(target.getAttribute('data-x')) || 0),
+                y = (parseFloat(target.getAttribute('data-y')) || 0);
+
+            // update the element's style
+            target.style.width = event.rect.width + 'px';
+            target.style.height = event.rect.height + 'px';
+
+            // translate when resizing from top or left edges
+            x += event.deltaRect.left;
+            y += event.deltaRect.top;
+
+            target.style.webkitTransform = target.style.transform =
+                'translate(' + x + 'px,' + y + 'px)';
+
+            target.setAttribute('data-x', x);
+            target.setAttribute('data-y', y);
+            target.textContent = Math.round(event.rect.width) + '×' + Math.round(event.rect.height);
+        });;
+});
+
