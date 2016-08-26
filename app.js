0ff22776733c671cef9a1df9bcb2ab40a9806651
Tom <seriousbsnz@gmail.com>
2016-08-26 22:25:29 +0000

    cleanup

--- app.js
+++ app.js
@@ -1,34 +1,5 @@
 var app = angular.module('myApp', []);
 
-app.controller('MainCtrl', function ($scope) {
-
-    $scope.resize = function(evt,ui) {
-      //console.log (evt,ui);
-      $scope.w = ui.size.width;
-      $scope.h = ui.size.height;
-		}
-
-  });
-
-app.directive('resizable', function () {
-
-    return {
-        restrict: 'A',
-        scope: {
-            callback: '&onResize'
-        },
-        link: function postLink(scope, elem, attrs) {
-            elem.resizable();
-            elem.on('resize', function (evt, ui) {
-              scope.$apply(function() {
-                if (scope.callback) { 
-                  scope.callback({$evt: evt, $ui: ui }); 
-                }                
-              })
-            });
-        }
-    };
-  })
 
 app.controller('myCtrl', GuessTheNumberController);
 
