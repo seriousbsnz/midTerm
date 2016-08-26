da0dcc8c34e5955e68606e151932acad3f30f411
Tom <seriousbsnz@gmail.com>
2016-08-26 16:35:41 +0000

    image upload added

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-var app = angular.module('myApp', [])
+var app = angular.module('myApp', []);
 
-.controller('myCtrl', GuessTheNumberController);
-	
-function GuessTheNumberController($scope) {
-	$scope.verifyGuess = function () {
+app.controller('myCtrl', GuessTheNumberController);
+
+function GuessTheNumberController($scope)
+{
+	$scope.verifyGuess = function()
+	{
 		$scope.deviation = $scope.original - $scope.guess;
 		$scope.noOfTries = $scope.noOfTries + 1;
 		$scope.falseAnswer = $scope.deviaiton != 0;
 	}
-	$scope.initializeGame = function () {
+	$scope.initializeGame = function()
+	{
 		$scope.noOfTries = 0;
 		$scope.original = Math.floor((Math.random() * 100) + 1);
 		$scope.guess = null;
@@ -16,3 +19,46 @@ function GuessTheNumberController($scope) {
 	}
 	$scope.initializeGame();
 }
+
+app.controller('uploadController', UploadController);
+
+function UploadController($scope, fileReader)
+{
+	console.log(fileReader);
+	$scope.getFile = function()
+	{
+		$scope.progress = 0;
+		fileReader.readAsDataUrl($scope.file, $scope)
+			.then(function(result)
+			{
+				$scope.imageSrc = result;
+			});
+	};
+
+	$scope.$on("fileProgress", function(e, progress)
+	{
+		$scope.progress = progress.loaded / progress.total;
+	});
+
+};
+
+app.directive("ngFileSelect", function()
+{
+
+	return {
+		link: function($scope, el)
+		{
+
+			el.bind("change", function(e)
+			{
+
+				$scope.file = (e.srcElement || e.target).files[0];
+				$scope.getFile();
+			})
+
+		}
+
+	}
+
+
+})
