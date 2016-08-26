608170c9f1d05395be1f62d77578960a95abb2c7
Tom <seriousbsnz@gmail.com>
2016-08-26 15:36:26 +0000

    improved number game

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
-var myApp = angular.module('myApp', []);
+var app = angular.module('myApp', [])
 
-myApp.controller('myCtrl', function($scope, $log) {
-									
-	var randomNum = Math.floor(Math.random() * 101);
-	var hint = document.querySelector('#hint');
-	var userGuess = document.querySelector('#guess');
-	var submitGuess = document.querySelector('#submit');			
+.controller('myCtrl', GuessTheNumberController);
 	
-	$scope.submitGuess = function() {
-		if ( userGuess > randomNum ) {
-			hint.value === 'Lower';
-		} else if ( userGuess < randomNum ) {
-			hint.value == 'Higher';
-		} else {
-			hint.value == 'You got it';
-		}
-	};
-	
-	$scope.startGame = function() {	
-		return randomNum;												
-	};														
-			
-											
-														
-														
-														
-})
+function GuessTheNumberController($scope) {
+	$scope.verifyGuess = function () {
+		$scope.deviation = $scope.original - $scope.guess;
+		$scope.noOfTries = $scope.noOfTries + 1;
+		$scope.falseAnswer = $scope.deviaiton != 0;
+	}
+	$scope.initializeGame = function () {
+		$scope.noOfTries = 0;
+		$scope.original = Math.floor((Math.random() * 100) + 1);
+		$scope.guess = null;
+		$scope.deviation = null;
+	}
+	$scope.initializeGame();
+}
